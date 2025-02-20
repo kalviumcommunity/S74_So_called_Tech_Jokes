8a3f49aa4fd8e1ca485d3854eebf8dc55bc569bd
KISHORE S <s.rithikkumarcaprio@gmail.com>
2025-02-20 10:44:59 +0530

    saved

---
+++ Front-End/client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import './home.css';
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="content">
+        <h1>Welcome to ASAP Project</h1>
+        <p>Contact: S.Rithik@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
